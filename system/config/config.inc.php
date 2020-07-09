90035479e7fa907ee788b63397b667bb0c423690
AdriOlogia <eidrian747@gmail.com>
2020-07-09 14:16:04 +0000

    V0.2 Brevity Front - Dia Dependencia Arg

--- system/config/config.inc.php
+++ system/config/config.inc.php
@@ -9,7 +9,7 @@ define("JSON_HACELO_BONITO_CON_ARRAY",JSON_PRETTY_PRINT+JSON_NUMERIC_CHECK+JSON_
 define("INTERFACE_TYPE",'frontend');
 
 // Título por omisión.
-define("MAINTITLE","Digital solutions for businesses seeking to innovate");
+define("MAINTITLE","Brevity");
 
 // Archivos del tinglado
 define("DEFAULT_SITE_HEAD","head");
